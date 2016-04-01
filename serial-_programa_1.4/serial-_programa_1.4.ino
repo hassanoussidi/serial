e3b606be1583f6a3e2754e639d9d215384b76b47
hassanoussidi <assan1@outlook.es>
2016-04-01 19:23:48 +0200

    serial_programa_1.4

---
+++ serial-_programa_1.4/serial-_programa_1.4.ino
@@ -0,0 +1,29 @@
+/***********************************************
+               mosrar "Hello world"                      
+             cada 1s en la pantalla
+                 verticalmente                
+***********************************************/
+
+//******  Includes  *****************************
+
+//******  Variables  ****************************
+
+
+
+//******  Setup  ********************************
+void setup()
+{
+  Serial.begin(9600); // abre el Puerto serie, configurando la velocidad en 9600 bps
+  Serial.println("Escull el numero de l'operacio que vols realitzar?");  // visualiza "Hello world" en la pantalla con un salto de linea
+  Serial.println(" 1. Comprovar numero de compte credit");
+  Serial.println(" 2. Comprovar numero de compte bancari");
+  Serial.println(" 3. Buscar un digit de targeta de credit");
+}  
+
+//******  Loop  *********************************
+void loop()
+{
+
+  
+}
+
